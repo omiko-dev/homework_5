e9af2946296f01288dc93659c8d3042e8fc0f38b
omiko-dev <omikovalishvili0@gmail.com>
2023-11-05 03:37:09 +0400

    add validation

--- app/src/main/java/com/example/homework_5/RegisterFirstActivity.kt
+++ app/src/main/java/com/example/homework_5/RegisterFirstActivity.kt
@@ -3,20 +3,62 @@ package com.example.homework_5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.homework_5.databinding.ActivityRegisterFirstBinding
 
 class RegisterFirstActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterFirstBinding
+    private var emailRegex: Regex =  Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    private var passwordRegex: Regex = Regex("^\\S*\$")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterFirstBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.ibBack.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            goMain()
         }
 
+
+        binding.acbNext.setOnClickListener {
+            if(checkRegisterInfo()){
+
+                goRegisterSecond()
+            }
+        }
+    }
+
+    private fun goMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goRegisterSecond(){
+        val intent = Intent(this, RegisterSecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun checkRegisterInfo(): Boolean{
+        if( // nullable validator
+            binding.etEmail.text.isNullOrBlank() ||
+            binding.etPassword.text.isNullOrBlank()
+        ){
+            return false
+        }
+        if( // email validator
+            !emailRegex.containsMatchIn(binding.etEmail.text.toString())
+        ){
+            return false
+        }
+        if( //password validator
+            binding.etPassword.text.toString().trim().contains(" ") ||
+            binding.etPassword.text.toString().length < 6
+        ){
+            return false
+        }
+
+        return  true
     }
 }
--- app/src/main/java/com/example/homework_5/RegisterSecondActivity.kt
+++ app/src/main/java/com/example/homework_5/RegisterSecondActivity.kt
@@ -1,11 +1,39 @@
 package com.example.homework_5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.homework_5.databinding.ActivityRegisterSecondBinding
 
 class RegisterSecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterSecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_second)
+        binding = ActivityRegisterSecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ibBack.setOnClickListener {
+            goRegisterFirst()
+        }
+
+
+        binding.acbSignUp.setOnClickListener {
+            if(checkUserName()){
+                Toast.makeText(this@RegisterSecondActivity, "wow", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun goRegisterFirst(){
+        val intent = Intent(this, RegisterFirstActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun checkUserName(): Boolean{
+        return binding.etUserName.text.toString().length > 6
     }
 }
